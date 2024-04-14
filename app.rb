d8c8c628bae9faefb6a2d2bc8e0d8708ab6a679a
yusufmuhumed <yusuf.muhumed@elev.ga.ntig.se>
2024-04-14 21:34:31 +0200

    mvc struktur

--- app.rb
+++ app.rb
@@ -64,21 +64,18 @@ get('/showlogout') do
 end
 
 post("/locked/new") do
- username= params[:username]
- password= params[:password]
- email= params[:email]
- password_confirm= params[:password_comfirm]
+  username= params[:username]
+  password= params[:password]
+  email= params[:email]
+  password_confirm= params[:password_comfirm]
 
-  if (password == password_confirm)
-    password_digest= BCrypt::Password.create(password)
-    db = SQLite3::Database.new('db/onepiece.db')
-    db.execute("INSERT INTO users (user_name,user_pwd,user_mail) VALUES(?,?,?)",username,password_digest,email)
+  if register(username,password,email,password_confirm) 
     redirect('/access')
-
   else
-    "fel lösenord"
-    
+    redirect('/')
   end
+
+  
 end
 
 
@@ -87,10 +84,8 @@ end
 get('/ranks/index') do
   @user_status = session[:user_status]
 
-  db = SQLite3::Database.new('db/onepiece.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Characters ORDER BY likes DESC")
-  @characters = db.execute("SELECT * FROM Characters")
+  result = ranks()
+  @characters = list_characters_info()
   
   liked_characters = session[:liked_characters] || []
   unliked_characters = session[:unliked_characters] || []
@@ -114,13 +109,7 @@ end
 get('/ranks/:id') do
   @user_status = session[:user_status]
   id = params[:id].to_i
-  p id
-
-  db = SQLite3::Database.new("db/onepiece.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Characters WHERE id = ?",id).first
-  #result2 = db.execute("SELECT Name FROM artists WHERE ArtistID IN (SELECT ArtistId FROM albums WHERE AlbumId = ?)",id).first
-  p "resultatet är: #{result}"
+  result= character(id)
   slim(:"ranks/show",locals:{result:result,user_status:@user_status})
 end
 
@@ -131,9 +120,7 @@ post('/like/:id') do
   status = 0
   id = params[:id].to_i
   
-  db = SQLite3::Database.new("db/onepiece.db")
-  db.results_as_hash = true
-  db.execute("UPDATE characters SET likes = likes + 1 WHERE id = ?", id)
+  like(id)
   session[:liked_characters] ||= []
   session[:liked_characters] << id unless session[:liked_characters].include?(id)
   redirect(:'/ranks/index')
@@ -141,11 +128,9 @@ post('/like/:id') do
 end
 
 post('/unlike/:id') do
-  id = params[:id].to_i
-  db = SQLite3::Database.new("db/onepiece.db")
-  db.results_as_hash = true
   status = 1
-  db.execute("UPDATE Characters SET likes = likes - 1 WHERE id = ?", id)
+  id = params[:id].to_i
+  unlike(id)
   session[:unliked_characters] ||= []
   session[:unliked_characters] << id unless session[:unliked_characters].include?(id)
   redirect(:'/ranks/index')
@@ -159,9 +144,7 @@ end
 
 post('/search/start') do
   name= params[:name]
-  db = SQLite3::Database.new('db/onepiece.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Characters WHERE name LIKE ?", (name + '%'))
+  result = search(name)
   p result
   session[:search_results] = result
   redirect(:"search/index")
@@ -179,10 +162,7 @@ end
 get('/search/:name') do
   @user_status = session[:user_status]
   name=params[:name]
-  db = SQLite3::Database.new("db/onepiece.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Characters WHERE name = ?",name).first
-  
+  result= select_name_where(name)
 
   slim(:"search/show",locals:{result:result,user_status:@user_status})
 end
@@ -191,13 +171,7 @@ end
 get('/lists/index') do
   @user_status = session[:user_status]
   user_id = session[:id]
-  db = SQLite3::Database.new("db/onepiece.db")
-  db.results_as_hash = true
-  results = db.execute("SELECT users.user_name, Characters.name
-  FROM((CharactersUsersRelations
-    INNER JOIN users ON CharactersUsersRelations.UsersId = users.id)
-    INNER JOIN Characters ON CharactersUsersRelations.CharactersId = Characters.id)
-  WHERE UsersId = ?", user_id)
+  results = user_list(user_id)
   p "test"
  
   slim(:"/lists/index", locals:{results:results,user_status:@user_status})
@@ -208,11 +182,7 @@ get('/lists/see-more') do
   user_id = session[:id]
   db = SQLite3::Database.new("db/onepiece.db")
   db.results_as_hash = true
-  results = db.execute("SELECT users.user_name, Characters.name
-  FROM((CharactersUsersRelations
-    INNER JOIN users ON CharactersUsersRelations.UsersId = users.id)
-    INNER JOIN Characters ON CharactersUsersRelations.CharactersId = Characters.id)
-  WHERE UsersId = ?", user_id)
+  results = user_list(user_id)
   p "test"
   p results
   slim(:"/lists/see-more", locals:{results:results,user_status:@user_status})
@@ -221,22 +191,7 @@ end
 post('/list/add') do
   name = params["name"]
   userId = session[:id]
-  db = SQLite3::Database.new('db/onepiece.db')
-  if is_name_a_character(name)
-    nameId = db.execute("SELECT id FROM Characters WHERE name = ?",name).first[0]
-    if name_already_in_list(nameId,userId) 
-      p "name already in list"
-      flash[:notice] = "Charcater already in list"
-
-    else
-      db.execute("INSERT INTO CharactersUsersRelations (CharactersId,UsersId) VALUES(?,?)",nameId,userId)
-    end
-  else
-    p "character dosen't exist"
-    flash[:notice] = "Character dosen't exist"
-
-    
-  end
+  add_character_to_list(name,userId)
   redirect(:'/lists/index')
 
 end
@@ -248,18 +203,17 @@ end
 
 post('/delete/:name') do
   p "hello"
-  name = params[:name]
+  character_name = params[:name]
   userId = session[:id]
   db1 = SQLite3::Database.new('db/onepiece.db')
   db = SQLite3::Database.new('db/onepiece.db')
 
   db1.results_as_hash = true
 
-  nameId = db1.execute("SELECT id FROM Characters WHERE name=?",name).first['id']
+  nameId = name_to_id(character_name)
 
-  p nameId
+  delete_character_from_list(nameId,userId)
   
-  db.execute("DELETE FROM CharactersUsersRelations WHERE CharactersId=? AND UsersId=?",nameId,userId)
   redirect(:'/lists/index')
 end
 
@@ -271,9 +225,7 @@ end
 
 post('/delete/:name') do
   name = params[:name]
-  db = SQLite3::Database.new('db/onepiece.db')
-  db.results_as_hash = true
-  db.execute("DELETE FROM Characters WHERE name=?",name)
+  delete_character(name)
   redirect(:"/ranks/index")
 end
 
--- model.rb
+++ model.rb
@@ -1,6 +1,7 @@
 require "sqlite3"
 require "bcrypt"
 
+
 def connect_to_db(path)
     db = SQLite3::Database.new(path)
     db.results_as_hash = true
@@ -13,6 +14,12 @@ def list_name()
    result = db.execute("SELECT name FROM Characters")
    return result
  
+end
+
+def list_characters_info()
+   db = SQLite3::Database.new('db/onepiece.db')
+   characters = db.execute("SELECT * FROM Characters")
+   return characters
 end
 
 def user(username,email)
@@ -78,6 +85,110 @@ def is_name_a_character(name)
 end
 
 
+def register(username,password,email,password_confirm)
+   p "hello"
+   if password == password_confirm
+      password_digest= BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/onepiece.db')
+      db.execute("INSERT INTO users (user_name,user_pwd,user_mail) VALUES(?,?,?)",username,password_digest,email)
+      return true
+  
+   else
+      return false
+   
+   end
+end
+
+def ranks()
+   db = SQLite3::Database.new('db/onepiece.db')
+   db.results_as_hash = true
+   result = db.execute("SELECT * FROM Characters ORDER BY likes DESC")
+   return result
+end
+
+
+def character(id)
+   db = SQLite3::Database.new("db/onepiece.db")
+   db.results_as_hash = true
+   result = db.execute("SELECT * FROM Characters WHERE id = ?",id).first
+   return result
+end
+
+def like(id)
+   db = SQLite3::Database.new("db/onepiece.db")
+   db.results_as_hash = true
+   db.execute("UPDATE characters SET likes = likes + 1 WHERE id = ?", id)
+end
+
+def unlike(id)
+   db = SQLite3::Database.new("db/onepiece.db")
+   db.results_as_hash = true
+   db.execute("UPDATE Characters SET likes = likes - 1 WHERE id = ?", id)
+end
+
+def search(name)
+   db = SQLite3::Database.new('db/onepiece.db')
+   db.results_as_hash = true
+   result = db.execute("SELECT * FROM Characters WHERE name LIKE ?", (name + '%'))
+   return result
+end
+
+def select_name_where(name)
+   db = SQLite3::Database.new("db/onepiece.db")
+   db.results_as_hash = true
+   result = db.execute("SELECT * FROM Characters WHERE name = ?",name).first
+   return result
+end
+
+def user_list(user_id)
+   db = SQLite3::Database.new("db/onepiece.db")
+   db.results_as_hash = true
+   results = db.execute("SELECT users.user_name, Characters.name
+   FROM((CharactersUsersRelations
+      INNER JOIN users ON CharactersUsersRelations.UsersId = users.id)
+      INNER JOIN Characters ON CharactersUsersRelations.CharactersId = Characters.id)
+   WHERE UsersId = ?", user_id)
+   return results
+end
+
+def add_character_to_list(name,userId)
+   db = SQLite3::Database.new('db/onepiece.db')
+   if is_name_a_character(name)
+      nameId = db.execute("SELECT id FROM Characters WHERE name = ?",name).first[0]
+      if name_already_in_list(nameId,userId) 
+         p "name already in list"
+         flash[:notice] = "Charcater already in list"
+
+      else
+         db.execute("INSERT INTO CharactersUsersRelations (CharactersId,UsersId) VALUES(?,?)",nameId,userId)
+      end
+   else
+      p "character dosen't exist"
+      flash[:notice] = "Character dosen't exist"
+
+    
+   end
+end
+
+def name_to_id(name)
+   db = SQLite3::Database.new('db/onepiece.db')
+   db.results_as_hash = true
+   nameId = db.execute("SELECT id FROM Characters WHERE name=?",name).first['id']
+   return nameId
+end
+
+
+def delete_character_from_list(nameId,userId)
+   db = SQLite3::Database.new('db/onepiece.db')
+   db.execute("DELETE FROM CharactersUsersRelations WHERE CharactersId=? AND UsersId=?",nameId,userId)
+end
+
+
+def delete_character(name)
+   db = SQLite3::Database.new('db/onepiece.db')
+   db.results_as_hash = true
+   db.execute("DELETE FROM Characters WHERE name=?",name)
+end
 
 
 
